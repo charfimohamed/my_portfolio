e6cb8b6ced79dab9d8487c1e80550553acec9554
mohamed charfi <100835078+charfimohamed@users.noreply.github.com>
2024-09-25 15:32:00 +0200

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -3,6 +3,6 @@ export const aboutData = {
     description1: "My name is Mohamed, I'm a master degree student at EPFL.",
     description2: "I am a passionate data science master's student with a strong interest in turning data into useful insights. I enjoy working on projects involving machine learning, data analysis, and visualization.\n" +
         "\n" +
-        "I am especially interested in applying data science to solve real-world problems in different fields like healthcare or finance. I am eager to join a team where I can continue learning and contribute to exciting projects.",
+        "I am especially interested in applying data science to solve real-world problems in different fields like healthcare, finance, market analysis etc. I am eager to join a team where I can continue learning and contribute to exciting projects.",
     image: 1
 }
