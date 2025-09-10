dff40087896dd7af9d84e8e774cccadb945f3aa1
mohamed charfi <100835078+charfimohamed@users.noreply.github.com>
2025-09-10 11:27:16 +0200

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,28 +1,35 @@
 export const experienceData = [
     {
         id: 1,
+        company: 'MAYA Private Capital Investments',
+        jobtitle: 'Data Scientist (Intern)',
+        startYear: 'Sep 2024',
+        endYear: 'Present'
+    },
+    {
+        id: 2,
         company: 'philip morris international',
         jobtitle: 'Data Analyst (Intern)',
         startYear: 'Feb 2025',
-        endYear: 'Present'
+        endYear: 'Aug 2025'
     },
     
     {
-        id: 2,
+        id: 3,
         company: 'Dnext',
         jobtitle: 'Junior Data Analyst (Intern)',
         startYear: 'Jul 2024',
         endYear: 'Feb 2025'
     },
     {
-        id: 3,
+        id: 4,
         company: 'EPFL',
         jobtitle: 'Teaching assistant (object oriented programming course)',
         startYear: 'Sep 2023',
         endYear: 'Feb 2024'
     },
     {
-        id: 4,
+        id: 5,
         company: 'ST2i',
         jobtitle: 'Junior fullstack Developer (Intern)',
         startYear: 'Jul 2023',
